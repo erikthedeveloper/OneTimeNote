8b370ce67b213636af0f6ba7b025cb6192fa93ce
Andrew Del Prete <andy@pathsofdesign.com>
2014-02-16 00:15:07 -0800

    Added Jeffrey Way's Laravel-Model-Validation to composer.jss

--- laravel/app/config/app.php
+++ laravel/app/config/app.php
@@ -106,7 +106,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-	    'OneTimeNote\OneTimeNoteServiceProvider'
+	    'OneTimeNote\OneTimeNoteServiceProvider',
 	),
 
 	/*
@@ -172,7 +172,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-
-	),
+        'Model'           => 'Way\Database\Model'
+    ),
 
 );
