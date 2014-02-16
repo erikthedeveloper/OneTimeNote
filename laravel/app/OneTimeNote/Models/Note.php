086077a4d71879225daa9ac1732d5d2d83b6f704
Andrew Del Prete <andy@pathsofdesign.com>
2014-02-15 22:50:55 -0800

    generated phpDocs for Note model

--- laravel/app/OneTimeNote/Models/Note.php
+++ laravel/app/OneTimeNote/Models/Note.php
@@ -1,6 +1,17 @@
 <?php
 namespace OneTimeNote\Models;
 
+/**
+ * An Eloquent Model: 'OneTimeNote\Models\Note'
+ *
+ * @property integer $id
+ * @property string $secure_note
+ * @property string $email
+ * @property string $ip_address
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property string $url_id
+ */
 class Note extends \Eloquent {
 
 	protected $hidden = array('id', 'ip_address', 'url_id', 'email', 'updated_at');
