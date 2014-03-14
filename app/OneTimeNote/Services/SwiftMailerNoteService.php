0632a3a3233ec55290dc1ec202e03250e99b4bc4
Andrew Del Prete <andy@pathsofdesign.com>
2014-03-13 16:31:15 -0800

    Added the Site Implementation URL to the Mailer template

--- app/OneTimeNote/Services/SwiftMailerNoteService.php
+++ app/OneTimeNote/Services/SwiftMailerNoteService.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace OneTimeNote\Services;
-
+use Illuminate\Support\Facades\Config;
 use OneTimeNote\Interfaces\NoteMailerInterface;
 use Illuminate\Support\Facades\Mail;
 
 class SwiftMailerNoteService implements NoteMailerInterface {
 
     public function to($email, $subject) {
-        Mail::send(array('html' => 'mailer.default'), array(), function($message) use ($email, $subject)
+        $data = array('SITE_IMPLEMENTATION_URL' => Config::get('onetimenote.SITE_IMPLEMENTATION_URL'));
+
+        Mail::send(array('html' => 'mailer.default'), $data, function($message) use ($email, $subject)
         {
             $message->to($email)->subject($subject);
         });
