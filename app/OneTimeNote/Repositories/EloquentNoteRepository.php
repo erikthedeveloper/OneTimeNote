5d26f7d89c372aa06f3206840fe699d7f0a62245
Erik Aybar <erikthedeveloper@gmail.com>
2014-07-05 15:53:24 -0600

    Minor refactor, dependency clarification, and adds method doc blocks
    - Extracts instances of `\Class` to `use Class`;
    - Consolidates `find()`
    - Adds possible TODOs via `@todo`
    - Removes `Str` facade in favor of `str_random()` helper method
    - Clarifies what is being returned from functions
        - `delete()` was not returning anything
        - Clarifies reason for assumed success on `deleteNotesOlderThan()`
    - Whitespace/Alignment

--- app/OneTimeNote/Repositories/EloquentNoteRepository.php
+++ app/OneTimeNote/Repositories/EloquentNoteRepository.php
@@ -2,82 +2,110 @@
 
 namespace OneTimeNote\Repositories;
 
-use OneTimeNote\Interfaces\NoteRepositoryInterface;
+use Exception;
+
+use Request;
 use Illuminate\Encryption\Encrypter;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Carbon\Carbon;
+
+use OneTimeNote\Interfaces\NoteRepositoryInterface;
 use OneTimeNote\Models\Note;
 
 class EloquentNoteRepository implements NoteRepositoryInterface {
 
+    /**
+     * Find one note by url_id and key
+     * @todo Implement more useful action on various exceptions being thrown
+     *
+     * @param  integer $url_id
+     * @param  string  $key
+     * @return OneTimeNote\Models\Note
+     */
     public function find($url_id, $key)
     {
-        $note = Note::where('url_id', '=', $url_id)->first();
-
-        if (!$note) {
-           return null;
-        }
-
         $encryption = new Encrypter($url_id . $key);
 
         try {
-            $note->secure_note = $encryption->decrypt($note->secure_note);
-        } catch (\Exception $e) {
+            $note              = Note::where('url_id', '=', $url_id)->firstOrFail();
+            $note->secure_note = nl2br( $encryption->decrypt($note->secure_note) );
+        } catch (ModelNotFoundException $e) {
+            return null;
+        } catch (Exception $e) {
             return null;
         }
-
-        $note->secure_note = nl2br($note->secure_note);
 
         return $note;
     }
 
+    /**
+     * Create a new note
+     * @param  array $input Associative array
+     * @return OneTimeNote\Models\Note
+     */
     public function create($input)
     {
-        $url_id = \Str::random(16);
-        $key = \Str::random(16);
+        $url_id     = str_random(16);
+        $key        = str_random(16);
         $encryption = new Encrypter($url_id . $key);
 
-        $note = new Note;
-        $note->fill($input);
-        $note->ip_address = \Request::getClientIp();
-        $note->url_id = $url_id;
+        $note              = new Note($input);
+        $note->ip_address  = Request::getClientIp();
+        $note->url_id      = $url_id;
         $note->secure_note = $encryption->encrypt($input['secure_note']);
+        $note->key         = $key;
 
         if (!$note->save()) {
+            /**
+             * @todo throw new Exception("Error creating note...", 1);
+             */
             return null;
         }
-
-        $note->key = $key;
 
         return $note;
     }
 
+    /**
+     * Delete a note by id
+     * @param  integer $id
+     * @return bool
+     */
     public function delete($id)
     {
         $note = Note::find($id);
 
         if (!$note) {
-            return null;
+            /**
+             * @todo throw new Exception("This should not be called by accident... MaliciousIntentException ?", 1);
+             */
+            return false;
         }
 
-        $note->delete();
+        $deleted = $note->delete();
+        return $deleted;
     }
 
+    /**
+     * @param  integer $days
+     * @return bool
+     */
     public function deleteNotesOlderThan($days)
     {
-        $date = new \Carbon\Carbon();
-        $date = $date->subDays($days);
+        $date        = new Carbon;
+        $target_date = $date->subDays($days);
+        $deleted     = Note::where('created_at', "<", $target_date)->delete();
 
-        $notes = Note::where('created_at', "<", $date);
-
-        if ($notes->count() == 0) {
-            return null;
-        }
-
-        $notes->delete();
-
+        /**
+         * Is considered success even if delete() returns false. (i.e. there were no notes within the range to delete)
+         */
         return true;
     }
 
+    /**
+     * Attempt to retrieve most recently created note for returning user
+     * @return OneTimeNote\Models\Note
+     */
     public function existingNoteByIpAddress() {
-        return Note::where('ip_address', '=', \Request::getClientIp())->orderBy('created_at', 'desc')->first();
+        return Note::where('ip_address', '=', Request::getClientIp())->orderBy('created_at', 'desc')->first();
     }
 }
